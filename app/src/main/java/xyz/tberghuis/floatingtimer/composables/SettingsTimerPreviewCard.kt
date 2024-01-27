1c4db015e75c2d60da847adb60e05fef288fa4d4
kuuuube <hexagonisalie@gmail.com>
2024-01-26 23:52:41 -0500

    Change timer font color when running and paused

--- app/src/main/java/xyz/tberghuis/floatingtimer/composables/SettingsTimerPreviewCard.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/composables/SettingsTimerPreviewCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -44,7 +45,7 @@ fun SettingsTimerPreviewCard(vmc: SettingsTimerPreviewVmc) {
         Box(
           contentAlignment = Alignment.Center,
         ) {
-          CountdownViewDisplay(vmc, 0.6f, 59)
+          CountdownViewDisplay(vmc, 0.6f, 59, Color(0xFF888888))
         }
       }
     }
